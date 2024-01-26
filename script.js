05cf8cf970a6c9f0bd77f5631186a2833d854d5b
musty4real2 <musty4real2@gmail.com>
2024-01-26 21:16:54 +0100

    black jack test

---
+++ script.js
@@ -0,0 +1,15 @@
+let firstCard = 6;
+let secondCard = 9;
+
+let sum = firstCard + secondCard;
+
+// console.log(sum);
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card? 🙂");
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳");
+} else {
+    console.log("You're out of the game! 😭");
+}
+
