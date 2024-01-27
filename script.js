3447330c5e1ad092fc9c648cef243a111de393e1
musty4real2 <musty4real2@gmail.com>
2024-01-27 22:09:49 +0100

    black jack interface

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-let firstCard = 6;
-let secondCard = 9;
-
+let firstCard = 10;
+let secondCard = 11;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message='';
 
-// console.log(sum);
-
-if (sum < 21) {
-    console.log("Do you want to draw a new card? 🙂");
+if (sum <= 20) {
+    message ="Do you want to draw a new card? 🙂";
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳");
+    message ="Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
 } else {
-    console.log("You're out of the game! 😭");
+    message ="You're out of the game! 😭";
+    isAlive = false;
 }
 
+// CASH OUT!
+// console.log(hasBlackJack);
+
+console.log(message);
--- testing.js
+++ testing.js
@@ -1,4 +1,4 @@
-let age = 20
+// let age = 20
 
 // if less than 21 -> "You can not enter the club!"
 // else            -> "Welcome!"
@@ -11,16 +11,17 @@ let age = 20
 
 
 
-let age = 145
+let age = 145;
 
 // if less than 100    -> "Not elegible"
 // else if exactly 100 -> "Here is your birthday card from the King!"
 // else                -> "Not elegible, you have already gotten one"
 
 if (age < 100) {
-    console.log("Not elegible")
+    console.log("Not elegible");
 } else if (age === 100) {
-    console.log("Here is your birthday card from the King!")
+    console.log("Here is your birthday card from the King!");
 } else {
-    console.log("Not elegible, you have already gotten one")
+    console.log("Not elegible, you have already gotten one");
+
 }
