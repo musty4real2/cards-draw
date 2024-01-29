d6ade9cf907fa69333dde6b1f82798af7bcc40e7
musty4real2 <musty4real2@gmail.com>
2024-01-29 22:02:50 +0100

    message

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let player = {
+    name: "Per",
+    chips: 200
+}
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,15 +10,19 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
-    let randomNumer = Math.floor( Math.random()*13 ) + 1
-    if (randomNumer > 10) {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
         return 10
-    } else if (randomNumer === 1) {
+    } else if (randomNumber === 1) {
         return 11
     } else {
-        return randomNumer
+        return randomNumber
     }
 }
 
@@ -54,5 +62,4 @@ function newCard() {
         cards.push(card)
         renderGame()        
     }
-
 }
